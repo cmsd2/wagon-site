0ceb093e08b465875707fdf23ddea3cfe453f34a
Chris Dawes <cmsd2@cantab.net>
2020-09-10 21:45:40 +0100

    add distribution

--- infra/lib/wagon-site-stack.ts
+++ infra/lib/wagon-site-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import { Duration } from "@aws-cdk/core";
 
 export interface WagonSiteProps extends cdk.StackProps {
@@ -9,6 +10,7 @@ export interface WagonSiteProps extends cdk.StackProps {
 
 export class WagonSiteStack extends cdk.Stack {
   public readonly bucket: s3.Bucket;
+  public readonly oai: cloudfront.IOriginAccessIdentity;
 
   constructor(scope: cdk.Construct, id: string, props: WagonSiteProps) {
     super(scope, id, props);
@@ -53,6 +55,49 @@ export class WagonSiteStack extends cdk.Stack {
       sources: [s3deploy.Source.asset("../build")],
       destinationBucket: this.bucket,
       serverSideEncryption: s3deploy.ServerSideEncryption.AES_256,
+    });
+
+    this.oai = new cloudfront.OriginAccessIdentity(this, "oai", {
+      comment: "wagon-site-bucket-oai",
+    });
+
+    this.bucket.grantRead(this.oai.grantPrincipal);
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "distribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: this.bucket,
+              originAccessIdentity: this.oai,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+              },
+            ],
+          },
+        ],
+        enableIpV6: true,
+        priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+        httpVersion: cloudfront.HttpVersion.HTTP2,
+      }
+    );
+
+    new cdk.CfnOutput(this, "wagon-site-distribution-domain-name", {
+      value: distribution.distributionDomainName,
+      exportName: "wagon-site-distribution-domain-name-" + props.envName,
+      description:
+        "wagon-site " + props.envName + " cloudfront distribution domain name",
+    });
+
+    new cdk.CfnOutput(this, "wagon-site-distribution-id", {
+      value: distribution.distributionId,
+      exportName: "wagon-site-distribution-id-" + props.envName,
+      description:
+        "wagon-site " + props.envName + " cloudfront distribution id",
     });
   }
 }
