b47dfb578b3129d5b8f69577e4231375c79ede5c
Chris Dawes <cmsd2@cantab.net>
2020-09-10 20:52:51 +0100

    change stack name to make resources match iam permissions

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,6 +4,6 @@ import * as cdk from "@aws-cdk/core";
 import { WagonSiteStack } from "../lib/wagon-site-stack";
 
 const app = new cdk.App();
-new WagonSiteStack(app, "WagonSiteStack", {
+new WagonSiteStack(app, "wagon-site-stack", {
   envName: "prod",
 });
